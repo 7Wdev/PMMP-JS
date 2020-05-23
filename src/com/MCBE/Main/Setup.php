43a75141a644d42ea4faf3afb15096dd52a1fee7
7Wdev <45117834+7Wdev@users.noreply.github.com>
2020-05-23 18:23:00 +0300

    a leak from the alpha version!

---
+++ src/com/MCBE/Main/Setup.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+namespace com\MCBE\Main;
+
+//pmmp libs!
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\Plugin;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\server\ServerCommandEvent;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+use pocketmine\lang\TranslationContainer;
+use ZipArchive;
+
+
+class Setup extends PluginBase implements Listener {
+
+    public function IsFolderExist($folder)
+      {
+        $path = realpath($folder);
+        return ($path !== false AND is_dir($path)) ? $path : false;
+      }
+
+    public function onEnable(): void
+      {
+         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+         $this->getServer()->getLogger()->info("Enabled By 7Wdev \nPMMP-JS loading js scripts ...");
+
+         //setup the Scripts dir!
+         $server_path = $this->getServer()->getDataPath();
+         $js_stations = "/Scripts";
+         $fucking_extra_var = $server_path . $js_stations;
+         if(!$this->IsFolderExist($fucking_extra_var))
+          {
+            mkdir($server_path . $js_stations, 0700);
+            $this->getServer()->getLogger()->info("Creating Scripts folder...");
+          } else
+              {
+                $this->getServer()->getLogger()->info("Scripts folder already created!");
+              }
+      }
+}
