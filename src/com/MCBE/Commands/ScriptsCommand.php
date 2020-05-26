d4844c05b15deaa4bbfddd214b2b544f55827b59
7Wdev <45117834+7Wdev@users.noreply.github.com>
2020-05-26 22:12:31 +0300

    Add files via upload

---
+++ src/com/MCBE/Commands/ScriptsCommand.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+namespace com\MCBE\Commands;
+
+/**************************************************************************************
+the code is working however it is messy,don't worry it wll be clean in the full version - STILL ALPHA1!
+**************************************************************************************/
+
+//pmmp libs!
+use pocketmine\plugin\Plugin;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\{Command, CommandSender, defaults\VanillaCommand};
+use pocketmine\command\CommandMap;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\server\ServerCommandEvent;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\lang\TranslationContainer;
+use ZipArchive;
+
+//needed functions!
+use function is_file;
+use function strlen;
+use function substr;
+
+//mine libs!
+use com\MCBE\Main\Setup;
+
+class ScriptsCommand {
+
+    private $command;
+
+    private $plugin;
+
+    public function __construct(Setup $plugin)
+      {
+        $this->command = $plugin;
+        $this->plugin = $plugin;
+      }
+
+    /////////////////////////////////////////////////////
+    /*this method can be used in addons of this project...*/
+    public function canLoadScript(string $path) : bool
+      {
+        $ext =".js";
+        return is_file($path) and substr($path, -strlen($ext)) === $ext;
+      }
+    /////////////////////////////////////////////////////
+
+    public function countScripts() : int
+      {
+        $dir = $this->plugin->getServer()->getDataPath() . "/Scripts/*.js";
+        $x = glob($dir);
+        return count($x);
+      }
+
+    public function onCommand(CommandSender $sender, Command $cmd, array $args) : bool
+      {
+        switch($cmd->getName())
+          {
+            case "scripts":
+            $LoadedScripts = $this->countScripts();
+            $sender->sendMessage(C::GREEN . 'The number of the loaded js scripts is : (' . $LoadedScripts . ')');
+          }
+        return true;
+      }
+}
---
+++ src/com/MCBE/Main/Setup.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+namespace com\MCBE\Main;
+
+/**************************************************************************************
+the code is working however it is messy,don't worry it wll be clean in the full version - STILL ALPHA1!
+**************************************************************************************/
+
+//pmmp libs!
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\Plugin;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\{Command, CommandSender, defaults\VanillaCommand};
+use pocketmine\command\CommandMap;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\server\ServerCommandEvent;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+use pocketmine\lang\TranslationContainer;
+use ZipArchive;
+
+//mine libs!
+use com\MCBE\Commands\ScriptsCommand;
+
+class Setup extends PluginBase implements Listener {
+
+    public static $instance;
+
+    public $server_path;
+
+    public $cmd;
+
+    public function IsFolderExist($folder)
+      {
+        $path = realpath($folder);
+        return ($path !== false AND is_dir($path)) ? $path : false;
+      }
+
+    public static function getInstance()
+      {
+        return self::$instance;
+      }
+
+    public function onLoad()
+      {
+        self::$instance = $this;
+      }
+
+    public function onEnable(): void
+      {
+         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+         $this->getServer()->getLogger()->info("Enabled By 7Wdev \nPMMP-JS loading js scripts ...");
+
+         //setup the Scripts dir!
+         $server_path = $this->getServer()->getDataPath();
+         $js_stations = "/Scripts";
+         $fucking_extra_var = $server_path . $js_stations;
+         if(!$this->IsFolderExist($fucking_extra_var))
+          {
+            mkdir($server_path . $js_stations, 0700);
+            $this->getServer()->getLogger()->info("Creating Scripts folder...");
+          } else
+              {
+                $this->getServer()->getLogger()->info("Scripts folder already created!");
+              }
+         $this->cmd = new ScriptsCommand($this);
+      }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool
+      {
+        return $this->cmd->onCommand($sender, $command, $args);
+     	}
+}
