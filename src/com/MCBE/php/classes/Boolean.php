6d1ec4e569ace07e44be45cc1b37b93d953695f4
7Wdev <45117834+7Wdev@users.noreply.github.com>
2020-06-20 22:34:21 +0300

    js2php convertor...

---
+++ src/com/MCBE/php/classes/Boolean.php
@@ -0,0 +1,68 @@
+<?php
+namespace com\MCBE\php\classes;
+
+/**************************************************************************************
+the code is working however it is messy,don't worry it wll be clean in the full version - STILL ALPHA1!
+also i wanna give credits for: https://github.com/sstur/js2php
+**************************************************************************************/
+
+class Bln extends Object {
+
+    public $className = "Boolean";
+    public $value = null;
+
+    static $protoObject = null;
+    static $classMethods = null;
+    static $protoMethods = null;
+
+    function __construct($value = null)
+      {
+        parent::__construct();
+        $this->proto = self::$protoObject;
+        if(func_num_args() === 1)
+         {
+           $this->value = $value;
+         }
+      }
+
+    /**
+    * Creates the global constructor used in user-land
+    * @return Func
+    */
+    static function getGlobalConstructor()
+      {
+        $Boolean = new Func(function($value = false)
+          {
+            $self = Func::getContext();
+            if($self instanceof Bln)
+             {
+               $self->value = $value ? true : false;
+               return $self;
+             } else
+                 {
+                   return $value ? true : false;
+                 }
+          });
+        $Boolean->instantiate = function()
+          {
+            return new Bln();
+          };
+        $Boolean->set('prototype', Bln::$protoObject);
+        $Boolean->setMethods(Bln::$classMethods, true, false, true);
+        return $Boolean;
+      }
+}
+
+Bln::$classMethods = array();
+Bln::$protoMethods = array('valueOf' => function()
+                                          {
+                                            $self = Func::getContext();
+                                            return $self->value;
+                                          }, 'toString' => function()
+                                                             {
+                                                               $self = Func::getContext();
+                                                               return to_string($self->value);
+                                                             });
+
+Bln::$protoObject = new Object();
+Bln::$protoObject->setMethods(Bln::$protoMethods, true, false, true);
