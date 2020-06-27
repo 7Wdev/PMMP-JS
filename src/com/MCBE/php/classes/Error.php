2993b73a7613fa32f536c9647585223fb083f816
7Wdev <45117834+7Wdev@users.noreply.github.com>
2020-06-27 14:01:56 +0300

    working on...

--- src/com/MCBE/php/classes/Error.php
+++ src/com/MCBE/php/classes/Error.php
@@ -1,4 +1,11 @@
 <?php
+namespace com\MCBE\php\classes;
+
+/**************************************************************************************
+the code is working however it is messy,don't worry it wll be clean in the full version - STILL ALPHA1!
+also i wanna give credits for: https://github.com/sstur/js2php
+**************************************************************************************/
+
 class Err extends Object {
   public $className = "Error";
   public $stack = null;
